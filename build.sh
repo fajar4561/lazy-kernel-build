2d5086fc5e1abfca03a04757a14eeea9f57786f6
fajar4561 <maulanafajar751@gmail.com>
2021-10-17 23:42:08 +0700

    kkkk

--- build.sh
+++ build.sh
@@ -59,13 +59,13 @@ NAMA=Signature
 
 JENIS=EAS
 
-VARIAN=OD
+VARIAN=LV&NVL
 # Build Type
 BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=clang
+COMPILER=gcc49
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
