7b414148eccffd997662b873b701c18208a6916f
fajar4561 <maulanafajar751@gmail.com>
2021-11-09 16:47:05 +0700

    u

--- build.sh
+++ build.sh
@@ -57,7 +57,7 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 # Kernel Variant
 NAMA=Signature
 
-JENIS=EAS
+JENIS=HMP
 
 VARIAN=NLV
 # Build Type
@@ -65,7 +65,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc49
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
