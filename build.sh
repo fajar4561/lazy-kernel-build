0aea68acdb06bb2ebe325ea8dc07e63053248e0e
fajar4561 <maulanafajar751@gmail.com>
2021-11-12 13:10:49 +0700

    Revert "j"
    This reverts commit adb79cb0f6e17724e7de6debb6c645305b09aa8b.

--- build.sh
+++ build.sh
@@ -65,7 +65,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc49
+COMPILER=gcc
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
