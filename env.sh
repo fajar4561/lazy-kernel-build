a0d32a044dbb27ecaea91bb56d251400c7e6246b
Thoreck <maulanafajar751@gmail.com>
2021-12-16 16:04:14 +0700

    clang

--- env.sh
+++ env.sh
@@ -31,7 +31,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=gcc49
+COMPILER=clang
 
 # Message on anykernel when installation
 MESSAGE="Salam dari kota congyang"
