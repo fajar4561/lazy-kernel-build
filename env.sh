51a1dbfe12b77d17eccef9406f4f3a6df8e206c0
Thoreck <maulanafajar751@gmail.com>
2022-11-20 07:59:59 +0700

    .....

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/fajar4561/Kernel_Asus_SDM660
 
 # Kernel Branch
-KERNEL_BRANCH=perf
+KERNEL_BRANCH=master-caf
 
 # The name of the device for which the kernel is built
 MODEL="Asus Zenfone Max Pro M1"
@@ -22,16 +22,16 @@ MANUFACTURERINFO="ASUSTek Computer Inc."
 # Kernel Variant
 NAMA=Signature
 
-JENIS=Mark
+JENIS=Experimental
 
-VARIAN=iii
+VARIAN=Mode
 
 # Build Type
 BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2'
-COMPILER=gcc49
+COMPILER=clangxgcc
 
 # Message on anykernel when installatio
 MESSAGE="Salam dari kota congyang"
