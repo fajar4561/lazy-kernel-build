5673e60af09fcbc05c6ceecb76118d365ae053a4
Thoreck <maulanafajar751@gmail.com>
2022-11-20 11:03:40 +0700

    %&

--- env.sh
+++ env.sh
@@ -31,7 +31,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2'
-COMPILER=gcc49
+COMPILER=gcc2
 
 # Message on anykernel when installatio
 MESSAGE="Salam dari kota congyang"
