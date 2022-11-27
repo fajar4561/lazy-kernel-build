c0125d6f52d5955d557752c19ac4db67b9b4e03c
Thoreck <maulanafajar751@gmail.com>
2022-11-27 11:43:38 +0700

    xpr

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/fajar4561/Kernel_Asus_SDM660
 
 # Kernel Branch
-KERNEL_BRANCH=R5
+KERNEL_BRANCH=base
 
 # The name of the device for which the kernel is built
 MODEL="Asus Zenfone Max Pro M1"
@@ -24,7 +24,7 @@ NAMA=Signature
 
 JENIS=IV-R5
 
-VARIAN=SE
+VARIAN=XPR
 
 # Build Type
 BUILD_TYPE="Nightly"
