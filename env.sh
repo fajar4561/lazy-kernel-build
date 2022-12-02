85d40b6a1349bf44ac383b4366b2f81fe61c87a6
Thoreck <maulanafajar751@gmail.com>
2022-12-02 20:36:05 +0700

    yuuuu

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/fajar4561/Kernel_Asus_SDM660
 
 # Kernel Branch
-KERNEL_BRANCH=R1
+KERNEL_BRANCH=R2
 
 # The name of the device for which the kernel is built
 MODEL="Asus Zenfone Max Pro M1"
