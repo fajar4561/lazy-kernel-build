6b1d9e31de10290b1ac26449d0bf90f8cde4216b
fajar <maulanafajar751@gmail.com>
2024-07-07 04:21:56 +0000

    u

--- env.sh
+++ env.sh
@@ -14,7 +14,7 @@ DEVICE="Sunfish"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=blu_spark_defconfig
+DEFCONFIG=sunfish_defconfig
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
@@ -40,7 +40,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER=clang
+COMPILER=gcc49
 
 # Message on anykernel when installatio
 MESSAGE="just flash and forget"
