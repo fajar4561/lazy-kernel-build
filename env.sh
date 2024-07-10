c4dc24b9ae12f4e93e877d5853868ef10c09c80e
fajar <maulanafajar751@gmail.com>
2024-07-10 04:34:16 +0000

    lld

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@
 KERNEL_REPO=https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/fajar4561/sunfish_kernel
 
 # Kernel Branch
-KERNEL_BRANCH=master
+KERNEL_BRANCH=1
 
 # The name of the device for which the kernel is built
 MODEL="Google Pixel 4a"
