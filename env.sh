abc0fa8b8bc3032bc56c78b64963bd26bfbf1799
fajar <maulanafajar751@gmail.com>
2024-07-10 06:54:37 +0000

    linaro

--- env.sh
+++ env.sh
@@ -40,7 +40,7 @@ BUILD_TYPE="Nightly"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49' , 'linaro & 'gcc2', clang2
-COMPILER=gcc49
+COMPILER=linaro
 
 # Message on anykernel when installatio
 MESSAGE="just flash and forget"
